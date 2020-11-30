0ddedc70d7814a85498bc375d88e0d4c8c542e3c
alex.g <aleks.g@digital-spectr.ru>
2020-11-30 11:18:41 +0500

    worked

--- src/App.vue
+++ src/App.vue
@@ -14,25 +14,38 @@
                 .simon__wrap-block
                     h1(style="text-align:center") Simon Game
                     ul.simon__control
-                        li.simon__control-item.blue( @click="sound(0)" :class="{_active: this.part === 0}")
-                        li.simon__control-item.red( @click="sound(1)" :class="{_active: this.part === 1}")
-                        li.simon__control-item.green( @click="sound(2)" :class="{_active: this.part === 2}")
-                        li.simon__control-item.yellow( @click="sound(3)" :class="{_active: this.part === 3}")
+                        li.simon__control-item.blue( @click="startPlayer(0)" :class="{_active: this.options.part === 0}")
+                        li.simon__control-item.red( @click="startPlayer(1)" :class="{_active: this.options.part === 1}")
+                        li.simon__control-item.green( @click="startPlayer(2)" :class="{_active: this.options.part === 2}")
+                        li.simon__control-item.yellow( @click="startPlayer(3)" :class="{_active: this.options.part === 3}")
+                        .simon__control-info
+                            span(v-if="!this.gameOver") {{options.round}}
+                            span(v-else) ПРОИГРЫШ
+
+
                 .simon__wrap-block
-                    .simon__option
-                        label.simon__option-item Легкий
-                            input(type="radio" name="level" value=1.5  :v-bind="this.level" checked)
-                        label.simon__option-item Средний
-                            input(type="radio" name="level" value=1 :v-bind="this.level")
-                        label.simon__option-item Сложный
-                            input(type="radio" name="level" value=0.4 :v-bind="this.level")
+                    .simon__wrap-block__option
+                        label.simon__option-item(for="begin") Легкий
+                            input(type="radio" name="level" id="begin" value=1.5  v-model.number="game.level" checked )
+                        label.simon__option-item(for="middle") Средний
+                            input(type="radio" name="level" id="middle" value=1 v-model.number="game.level")
+                        label.simon__option-item(for="hard") Сложный
+                            input(type="radio" name="level" id="hard" value=0.4 v-model.number="game.level")
                         label.simon__option-item
-                    //button.simon__btn(@click="this.btn === !this.bnt") Start
-                    button.simon__btn(@click="start") Start
+                    button.simon__btn(@click="btnStart = !btnStart" :class="{_active: this.btnStart}")
+                        span(v-if="!btnStart") start
+                        span(v-else) Stop
+                    h2 game
+                    .simon__data
+                        div selection {{this.game.selection}}
+                        div round {{this.game.round}}
+                        div options.round {{this.options.round}}
+                        h2 player
+                        div selection {{this.player.selection}}
+                        div round {{this.player.round}}
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
 
     export default {
         name: 'App',
@@ -43,45 +56,118 @@
                 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
                 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
             ],
-            play: false,
-            level: 1.5,
-            part: '',
-            btn: false,
-            step: 1
+            btnStart: false,
+            gameOver: false,
+            win: false,
+            options: {
+                play: false,
+                part: '',
+                round: 1,
+            },
+            game: {
+                round: 0,
+                selection: [],
+                level: 1.5,
+            },
+            player: {
+                round: 0,
+                selection: [],
+            }
         }),
-        components: {
-            HelloWorld
+        watch: {
+            btnStart: function () {
+                if (this.btnStart) {
+                    this.start()
+                }
+            },
+            gameOverEnd: function () {
+                if (this.gameOver) {
+                    this.game.selection = []
+                    this.player.selection = []
+                    return false
+                }
+            }
+
         },
         methods: {
-            sound(i) {
-                new Audio(this.sounds[i]).play()
-            },
             start() {
-                setTimeout(()=> {
-                    this.playing()
-                }, this.level * 1000)
+                this.game.selection = []
+                this.player.selection = []
+                this.playing()
             },
+
+            // ХОДИТ ИГРА
             playing() {
-                function randomInteger(min, max) {
-                    let rand = min + Math.random() * (max + 1 - min);
-                    return Math.floor(rand);
+                if (!this.btnStart) {
+                    return false
+                } else {
+                    setTimeout(() => {
+
+                        if (this.game.round !== this.options.round && !this.gameOver) {
+                            let stepPlay = this.randomStep(0, 3);
+                            this.visual(stepPlay)
+                            this.game.selection.push(stepPlay)
+                            this.game.round++
+                            this.playing()
+
+                        } else {
+                            this.clearPart()
+                        }
+
+                    }, this.game.level * 1000)
                 }
-                let i = (randomInteger(0, 3))
-                console.log(i)
-                this.part = i
+            },
+
+            // ХОДИТ ИГРОК
+            startPlayer(i) {
+                this.visual(i)
+                this.player.selection.push(i)
+                this.checked(i) // ПРОВЕРКА С ХОДОМ ИГРЫ
+            },
+
+            // ПРОВЕРКА
+            checked() {
+                let gameSelected = this.game.selection.slice()
+                let playerSelected = this.player.selection.slice()
+                let helps=0
+                for (let i = 0; i < playerSelected.length; i++) {
+                    // this.gameOver = gameSelected[i] !== playerSelected[i];
+                    if (gameSelected[i] === playerSelected[i]) {
+                        this.gameOver = false
+                    } else {
+                        this.gameOver = true
+                    }
+                    console.log('gameSelected[i] ',gameSelected[i] )
+                    console.log('playerSelected[i] ',playerSelected[i] )
+                    // console.log('gameSelected.length ',gameSelected.length)
+                    helps++
+                }
+                if (helps === gameSelected.length ) {
+                    console.log('все заебок')
+                    this.roundClear()
+                    this.playing()
+                }
+
+            },
+            roundClear() {
+                this.player.round++
+                this.options.round++
+                this.game.round = 0
+                this.game.selection = []
+                this.player.selection = []
+            },
+            randomStep(min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            },
+            clearPart() {
+                this.options.part = ''
+            },
+            visual(i) {
                 new Audio(this.sounds[i]).play()
-                this.start()
-                // setTimeout(() => this.start , 1000);
+                this.options.part = i
             }
         },
-        computed: {
-            // startPlaying(){
-            //     if (this.btn) {
-            //         return this.start()
-            //     }
-            //     return this.start()
-            // }
-        }
+
 
     }
 </script>
